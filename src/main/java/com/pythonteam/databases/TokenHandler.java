dc378d696f34bff38e961ca178e0af07ded8e882
Cristian Perez <creshandkesh@gmail.com>
2018-02-26 16:08:55 -0600

    añadido una parte de la creacion de token

--- src/main/java/com/pythonteam/databases/TokenHandler.java
+++ src/main/java/com/pythonteam/databases/TokenHandler.java
@@ -2,6 +2,7 @@ package com.pythonteam.databases;
 
 import com.pythonteam.models.Token;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,7 +14,19 @@ public class TokenHandler implements BaseHandler<Token>{
     }
 
     @Override
-    public Token findOne(int id) {
+    public Token findOne(int id) throws SQLException {
+        String sql = "select * from token where token = :token";
+        try (Connection conn = Database.getConnection())
+        {
+            NamedParameterStatement nps = new NamedParameterStatement(conn, sql);
+            nps.setInt("token",id);
+            ResultSet rs = nps.executeQuery();
+            if (rs.next()) {
+                Token t = fill(rs);
+                rs.close();
+                return t;
+            }
+        }
         return null;
     }
 
@@ -28,7 +41,14 @@ public class TokenHandler implements BaseHandler<Token>{
     }
 
     @Override
-    public boolean create(Token token) {
+    public boolean create(Token token) throws SQLException {
+        String sql = "INSERT INTO token(token) VALUES (:token);";
+        String tokenHash = "asD";
+        try (Connection conn = Database.getConnection()) {
+            NamedParameterStatement nps = new NamedParameterStatement(conn, sql);
+            nps.setString("token", tokenHash);
+            nps.executeQuery();
+        }
         return true;
     }
 
